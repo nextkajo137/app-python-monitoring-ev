e7158e52c4b8a628a9cec35341afdc9a31e45bc7
Gabriel <gabrielletris29@gmail.com>
2026-07-18 18:12:59 +0700

    Menambahkan Halaman Error (Tugas Poin 6)
    Deskripsi: Membuat halaman 403, 404, dan 500 sederhana sesuai dengan base template, serta mendaftarkan handler error 500 di backend.

--- app.py
+++ app.py
@@ -705,5 +705,10 @@ def forbidden(e):
     return render_template("errors/403.html"), 403
 
 
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template("errors/500.html"), 500
+
+
 if __name__ == "__main__":
     app.run(host=APP_HOST, port=APP_PORT, debug=True)
